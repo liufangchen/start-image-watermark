f51f224b5f7535a979b3200cd77717949e46a472
liufangchen <1171988327@qq.com>
2022-08-09 15:03:37 +0800

    update index.js

--- hook/index.js
+++ hook/index.js
@@ -3,7 +3,10 @@ async function preInit(inputObj) {
 }
 
 async function postInit(inputObj) {
-
+    console.log(`\n    Welcome to use start-image-watermark
+     * 部署前执行请安装go
+         go  : https://golang.google.cn/doc/install
+     * 项目初始化完成，您可以直接进入项目目录下，并使用 s deploy 进行项目部署\n`)
 }
 
 module.exports = {
